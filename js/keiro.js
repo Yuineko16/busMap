dbe3d1bd509753c96edebdfe7a551eb952b91480
Yuineko16 <72602795+Yuineko16@users.noreply.github.com>
2021-10-13 13:50:00 +0900

    Update keiro.js

--- js/keiro.js
+++ js/keiro.js
@@ -1,8 +1,12 @@
 var output = 0;
 function getTime(x, y) {
   var elem = document.getElementById("output");
-  t = (((x - y) / 60000 | 0) + 540)
-  return (t / 60 | 0) + ":" + t % 60;
+  var t = (((x - y) / 60000 | 0) + 540)
+  var m = t % 60;
+  if(m<10){
+    m = '0'+ m;
+  }
+  return (t / 60 | 0) + ":" + m;
 }
 
 var xmlhttp = new XMLHttpRequest();
@@ -19,8 +23,8 @@ xmlhttp.onreadystatechange = function () {
       var endTime = docelem.getElementsByTagName("endTime");
       //t=((startTime[0].textContent-date[1].textContent)/60000 | 0)+540
 
-      elem.innerHTML += "<div class='keiro_content'><p class='keiro_title'>ルート情報</p>"
-        + "<p>日付" + " : " + date[0].textContent + "</p>"
+      elem.innerHTML += "<div class='keiro-content'><p class='keiro-title'>ルート情報</p>"
+        + "<p>日付 : " + date[0].textContent + "</p>"
         + "<p>運賃" + " : " + cents[0].textContent + "</p>"
         + "<p>出発時間" + " : " + getTime(startTime[0].textContent, date[1].textContent) + "</p>"
         + "<p>到着時間" + " : " + getTime(endTime[0].textContent, date[1].textContent) + "</p>";
@@ -32,7 +36,7 @@ xmlhttp.onreadystatechange = function () {
       var arrival = docelem.getElementsByTagName("arrival");
       var departure = docelem.getElementsByTagName("departure");
 
-      elem.innerHTML += "<div class='keiro_content'><p class='keiro_title'>ルート詳細</p>";
+      elem.innerHTML += "<div class='keiro-content'><p class='keiro-title'>ルート詳細</p>";
 
 
       var j = 0;
@@ -41,33 +45,37 @@ xmlhttp.onreadystatechange = function () {
         if (name[i].textContent != "Origin" && name[i].textContent != "Destination") {
 
           if (i % 2 == 0) {
-            elem.innerHTML += "<p>路線" + ":" + route[j].textContent + "</p>"
-              + "<p>行先" + ":" + headsign[j].textContent + "</p>"
 
             if (route[j].textContent == "" && headsign[j].textContent == "") {
-              elem.innerHTML += "<p>移動手段" + ":" + "徒歩" + "</p>"
+              elem.innerHTML += "<p>移動手段" + " : " + "徒歩" + "</p>"
             }
             else {
-              elem.innerHTML += "<p>移動手段" + ":" + "バス・LRT" + "</p>"
+              elem.innerHTML += "<p>移動手段" + " : " + "バス・LRT" + "</p>"
+              elem.innerHTML += "<p>路線" + " : " + route[j].textContent + "</p>"
+              + "<p>行先" + " : " + headsign[j].textContent + "</p>"
             }
             j++;
           }
 
           if (arrival[i].textContent != "" && arrival[i].textContent != arrival[i - 1].textContent) {
-            elem.innerHTML += "<p>到着時間" + ":" + getTime(arrival[i].textContent, date[1].textContent) + "</p>";
+            elem.innerHTML += "<p>到着時間" + " : " + getTime(arrival[i].textContent, date[1].textContent) + "</p>";
           }
 
-          elem.innerHTML += "<p>駅名" + ":" + name[i].textContent + "</p>"
+          elem.innerHTML += "<p>駅名" + " : " + name[i].textContent + "</p>"
 
           if (departure[i].textContent != "" && departure[i].textContent != departure[i + 1].textContent) {
-            elem.innerHTML += "<p>発車時間" + ":" + getTime(departure[i].textContent, date[1].textContent) + "</p>";
+            elem.innerHTML += "<p>発車時間" + " : " + getTime(departure[i].textContent, date[1].textContent) + "</p>";
           }
 
-          if (i % 2 == 1) {
-            elem.innerHTML += "<p>-----乗り換え-----</p>";
+          
+
+          if (i % 2 == 1 &&  i!=name.length-1) {
+            elem.innerHTML += "<div class='blank'></div>";
+            elem.innerHTML += "</div><p>-----乗り換え-----</p>";
           }
         }
       }
+      elem.innerHTML += "<div class='blank'></div>";
       elem.innerHTML += "<p>到着</p>";
     } else {
       alert("status = " + xmlhttp.status);
